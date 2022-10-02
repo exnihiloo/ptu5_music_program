854bc24b81dfeb4acca7f30d491d353e5bd1c074
exnihiloo <deimante.jasaityte@gmail.com>
2022-10-02 19:15:31 +0300

    opening json file

--- quiz.py
+++ quiz.py
@@ -22,5 +22,14 @@ class Quiz:
 langas = Tk()
 langas.geometry("550x350")
 langas.title("\U0001F3A7 Music Quiz")
+# gauname duomenis iš json failo
+with open('data.json') as failas:
+    data = json.load(failas)
+ 
+# nustatome klausimus, pasirinkimus ir atsakymus, kurie yra json faile
+question = (data['question'])
+options = (data['options'])
+answer = (data[ 'answer'])
+
 quiz = Quiz()
 langas.mainloop()
