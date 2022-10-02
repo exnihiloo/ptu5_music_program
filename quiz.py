c96915a0ed0bee37dd4a6a5943183fb42116c12e
exnihiloo <deimante.jasaityte@gmail.com>
2022-10-02 19:15:05 +0300

    creating window with title

--- quiz.py
+++ quiz.py
@@ -4,4 +4,23 @@ import json
 
 class Quiz:
     def __init__(self):
-         pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+langas = Tk()
+langas.geometry("550x350")
+langas.title("\U0001F3A7 Music Quiz")
+quiz = Quiz()
+langas.mainloop()
