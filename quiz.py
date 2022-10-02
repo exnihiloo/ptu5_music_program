433c122fe2f14451fd569f8ada42e9a9aa4ebcfb
exnihiloo <deimante.jasaityte@gmail.com>
2022-10-02 19:19:29 +0300

    creatin methods radiobtn, btn, show q, show option

--- quiz.py
+++ quiz.py
@@ -4,7 +4,36 @@ import json
 
 class Quiz:
     def __init__(self):
+        # nustatome klausimų skaičių į 0
+        self.klausimai = 0
+        # nustatome rodomus klausimus
+        self.rodyti_klausima()
+        # pasirinktas_ats laiko int reikšmę, kuri naudojama
+        # pasirinktoms klausimų opcijoms.
+        self.pasirinktas_ats = IntVar()
+        # roddysime radio buttons esamam klausimui
+        # ir pateiksime klausimus prie radio button
+        self.opcija = self.radio_buttons() 
+        # rodysim pasiirnkimus klausimui
+        self.rodyti_pasirinkimus()
+        # "Kitas" ir "išeiti mygtukai"
+        self.mygtukai()
+        # nustatome klausimų kiekį. question yra iš data.json failo
+        self.viso_klausimu = len(question)
+        # skaičiuosime teisingus atsakymus.
+        self.teisingi = 0
 
+    def mygtukai(self):
+        pass
+
+    def rodyti_klausima(self):
+        pass
+
+    def rodyti_pasirinkimus(self):
+        pass
+
+    def radio_buttons(self):
+        pass
 
 
 
